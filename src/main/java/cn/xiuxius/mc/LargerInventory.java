fe9b8b342e5e0c641d5077cc0e35f57e2001044c
XiaoXiu <sh_xiaoxiu@qq.com>
2026-03-27 19:43:56 +0800

    init commit

---
+++ src/main/java/cn/xiuxius/mc/LargerInventory.java
@@ -0,0 +1,17 @@
+package cn.xiuxius.mc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LargerInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
